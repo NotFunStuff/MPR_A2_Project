3ead4c3744ab4ffb843c33aa25febe950d93ee8e
Duc Ta <taminhduc28092000@gmail.com>
2021-05-18 15:33:46 +0700

    RecycleView Main

--- MyCart/app/src/main/java/hanu/a2_1801040067/MyCart/adapter/ItemAdapter.java
+++ MyCart/app/src/main/java/hanu/a2_1801040067/MyCart/adapter/ItemAdapter.java
@@ -1,5 +1,7 @@
 package hanu.a2_1801040067.MyCart.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import hanu.a2_1801040067.MyCart.R;
@@ -28,9 +31,11 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
         private ImageView itemThumbnail;
         private TextView itemPrice;
         private ImageButton itemAddBtn;
+        private Context context;
 
-        public ItemHolder(@NonNull View itemView) {
+        public ItemHolder(@NonNull View itemView, Context context) {
             super(itemView);
+            this.context = context;
             itemName = itemView.findViewById(R.id.item_name);
             itemThumbnail = itemView.findViewById(R.id.item_thumbnail);
             itemPrice = itemView.findViewById(R.id.item_price);
@@ -40,23 +45,38 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
         public void bind(Product item){
             itemName.setText(item.getName());
             ImageLoad helper = new ImageLoad(item.getThumbnail(), itemThumbnail);
-            itemPrice.setText(item.getUnitPrice());
+            DecimalFormat dFormat = new DecimalFormat();
+            String formattedString = dFormat.format(item.getUnitPrice());
+            System.out.println(formattedString);
+            itemPrice.setText(formattedString + "VND");
+
+            itemAddBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
     }
     @NonNull
     @Override
     public ItemAdapter.ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View itemView = inflater.inflate(R.layout.item_shop, parent,false);
+        return new ItemHolder(itemView, context);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemAdapter.ItemHolder holder, int position) {
-
+        Product item = productList.get(position);
+        holder.bind(item);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return productList.size();
     }
 }
 
